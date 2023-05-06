4170b214e6723bb97c72f4cfc419aeb189abf979
Julien FRANCAIS <jfrancai@student.42.fr>
2023-05-06 14:59:56 +0200

    much better archi: I build the web site before running the server

--- src/app.ts
+++ src/app.ts
@@ -1,78 +1,48 @@
 import * as http from 'http';
-import * as fs from 'fs';
-import { readFile } from 'node:fs/promises';
+import { readFileSync, readdirSync, Dirent } from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import * as cheerio from 'cheerio';
-import MarkdownIt from 'markdown-it'
+
+const hostname: string = '127.0.0.1';
+const port: number = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const markdown: MarkdownIt = MarkdownIt({html: true});
-const hostname: string = '127.0.0.1';
-const port: number = 3000;
-
 const server = http.createServer();
 
-server.on('request', async (req: http.IncomingMessage, res: http.ServerResponse) => {
-	if (req.method === 'GET') {
-		let data: string | Buffer;
-		let status: number;
-		if (req.url === '/')
-		{
-			data = fs.readFileSync(__dirname + '/index.html');
-			status = 200;
-			const dirs: string[] | Buffer[] | fs.Dirent[] = fs.readdirSync(__dirname + '/zet');
+function getNoteUri(): string[] {
 
-			const $ = cheerio.load(data);
-			dirs.forEach((file: string) => {
-				$('ul').append(`<li><a href="${file}">${file} </a></li>`);
-			});
-			data = $.html();
-		}
-		else if (req.url)
-		{
-			try
-			{
-				data = await convertMarkdownToHtml(req.url);
-				status = 200;
-			}
-			catch (e: any)
-			{
-				status = 404;
-				data = "not found 0";
-			}
-		}
-		else
-		{
-			status = 404;
-			data = "not found 1";
-		}
-		res.writeHead(status, {
-			'Content-Type': 'text/html',
-		})
-		return res.end(data);
+	const dirs: string[] | Buffer[] | Dirent[] = readdirSync(__dirname + '/zet');
+
+	const ret: string[] = new Array(dirs.length);
+	dirs.forEach((file: string, index: number) => {
+		ret[index]	= file.substring(0, file.length - 5);
+	})
+	return ret;
+}
+
+server.on('request', async (req: http.IncomingMessage, res: http.ServerResponse) => {
+	let data: string | Buffer = "not found"; 
+	let status: number = 404;
+
+	let index: number = -1;
+	let uri: string = req.url ? req.url : "/";
+
+	const URIs: string[] = getNoteUri();
+
+	if (req.url == '/' && req.method === 'GET') {
+		data = readFileSync(__dirname + '/index.html');
 	}
+	else if ((index = URIs.indexOf(uri.substring(1, uri.length))) > -1) {
+		data = readFileSync(__dirname + '/zet/' + URIs[index] + '.html');
+	}
+	res.writeHead(status, {
+		'Content-Type': 'text/html',
+	})
+	return res.end(data);
 })
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 })
-
-async function convertMarkdownToHtml(zetId: string): Promise< string > {
-	try {
-		// mardown source
-		const content: string = await readFile(__dirname + "/zet"+ zetId + "/README.md", 'utf8');
-		const rendered = markdown.render(content);
-		const template: string | Buffer = fs.readFileSync(__dirname + '/template.html');
-		const $ = cheerio.load(template);
-		$('div').append(rendered);
-		return $.html();
-	}
-	catch (e: any)
-	{
-		console.log(e);
-		throw e;
-	}
-}
---
+++ src/build.ts
@@ -0,0 +1,40 @@
+import { writeFileSync, readFileSync, readdirSync, Dirent } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as cheerio from 'cheerio';
+import MarkdownIt from 'markdown-it'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const __src = __dirname + "/../src";
+
+const markdown: MarkdownIt = MarkdownIt({html: true});
+
+(() => {
+	const dirs: string[] | Buffer[] | Dirent[] = readdirSync(__src + '/zet');
+
+	const templateHtml: string | Buffer = readFileSync(__src + '/template.html');
+	const indexHtml: string | Buffer = readFileSync(__src + '/index.html');
+
+	const template = cheerio.load(templateHtml);
+	const index = cheerio.load(indexHtml);
+
+	dirs.forEach((folder: string) => {
+		try {
+			const notePath: string = __src + "/zet/" + folder + "/README.md";
+
+			const mdNote: string = readFileSync(notePath, 'utf8');
+			const htmlNote = markdown.render(mdNote);
+
+			template('div').append(htmlNote);
+			index('ul').append(`<li><a href="${folder}">${folder} </a></li>`);
+
+			writeFileSync(__dirname + '/zet/' + folder + ".html", template.html());
+		}
+		catch (e: any)
+		{
+			console.log(e);
+		}
+	});
+	writeFileSync(__dirname + '/index.html', index.html());
+})()
